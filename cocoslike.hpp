d24a8a686fe1e076d5060f6bebe1a590ca133b90
ユーマヨ <scha-taz0@outlook.jp>
2017-04-16 16:35:53 +0900

    すべての機能を読み込むヘッダーを作成。

---
+++ cocoslike.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "node.h"
+#include "scene.h"
+#include "scene_manager.h"
+#include "user_default.h"
+
+#include "audio.hpp"
+#include "renderer.hpp"
+#include "action.hpp"
+#include "network.hpp"
+#include "utility.hpp"
